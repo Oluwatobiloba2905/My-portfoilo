7f5b8283ff85e4b083f07ee3142fb60c758c018b
Alonge Oluwatobiloba <104198149+Oluwatobiloba2905@users.noreply.github.com>
2023-12-20 08:46:28 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: Personal - v2.1.0
-* Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 !(function($) {
   "use strict";
 
